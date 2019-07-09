744ba3b1681e702fdb8c16a09d3d75e8a95500e3
Cameron May <camerondmay@gmail.com>
2019-07-09 13:16:15 +1200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -162,6 +162,7 @@ function writeTextUI() {
 
   col = color(0, 0, 0, 0.1);
   colSelect = color(0, 0, 0, 1);
+  colH2 = color(230, 20, 74);
   colH3 = color(355, 87, 74);
 
   button1A.style('background-color', colSelect)
@@ -183,17 +184,17 @@ function writeTextUI() {
   button1C.style('width', '6vmax');
   button1C.style('border-radius', '0.5vmax')
   button1C.mousePressed(rake2);
-  button2.style('background-color', colH3);
-  button2.style('font-size', '2vmax');
-  button2.style('color', 'white');
-  button2.style('border-radius', '0.25vmax')
-  button2.style('width', '18vmax')
+  button2.style('background-color', colH2);
+  button2.style('font-size', '1.75vmax');
+  button2.style('color', 'black');
+  button2.style('border-radius', '0.5vmax')
+  button2.style('width', '10vmax')
   button2.mousePressed(enterFS);
   button3.style('background-color', colH3);
-  button3.style('font-size', '2vmax');
+  button3.style('font-size', '1.75vmax');
   button3.style('color', 'white');
-  button3.style('border-radius', '0.25vmax')
-  button3.style('width', '18vmax')
+  button3.style('border-radius', '0.5vmax')
+  button3.style('width', '12vmax')
   button3.mousePressed(resetTimeout);
 }
 
@@ -214,8 +215,8 @@ function mouseDragged() {
   //   blendMode(BLEND);
   //
   //   loadPixels();
-  //   for (let y = (mouseY - 20); y < (mouseY + 20); y++) {
-  //     for (let x = (mouseX - 20); x < (mouseX + 20); x++) {
+  //   for (let y = (winMouseY - 20); y < (winMouseY + 20); y++) {
+  //     for (let x = (winMouseX - 20); x < (winMouseX + 20); x++) {
   //       let index = (x + y * width) * 4;
   //       // Below, the reason for adding the existing pixels back on is to fake a 50%
   //       // opacity/alpha, which I suspect is not otherwise possible with a Pixel update
@@ -233,11 +234,11 @@ function mouseDragged() {
 
 ;
 
-    dx = mouseX - rake3X;
-    dy = mouseY - rake3Y;
+    dx = winMouseX - rake3X;
+    dy = winMouseY - rake3Y;
     angle1 = atan2(dy, dx);
-    rake3X = mouseX - (cos(angle1) * (segLength / 2));
-    rake3Y = mouseY - (sin(angle1) * (segLength / 2));
+    rake3X = winMouseX - (cos(angle1) * (segLength / 2));
+    rake3Y = winMouseY - (sin(angle1) * (segLength / 2));
 
     segment(rake3X, rake3Y, angle1, img_brush)
     // reference for brush offset at https://p5js.org/examples/interaction-follow-1.html
@@ -247,11 +248,11 @@ function mouseDragged() {
 
 
 
-    dx = mouseX - rakeX;
-    dy = mouseY - rakeY;
+    dx = winMouseX - rakeX;
+    dy = winMouseY - rakeY;
     angle1 = atan2(dy, dx);
-    rakeX = mouseX - (cos(angle1) * segLength);
-    rakeY = mouseY - (sin(angle1) * segLength);
+    rakeX = winMouseX - (cos(angle1) * segLength);
+    rakeY = winMouseY - (sin(angle1) * segLength);
 
     segment(rakeX, rakeY, angle1, img_rake)
   }
@@ -259,11 +260,11 @@ function mouseDragged() {
   if (bool_button1 === 2) {
 
 
-    dx = mouseX - rake2X;
-    dy = mouseY - rake2Y;
+    dx = winMouseX - rake2X;
+    dy = winMouseY - rake2Y;
     angle1 = atan2(dy, dx);
-    rake2X = mouseX - (cos(angle1) * segLength);
-    rake2Y = mouseY - (sin(angle1) * segLength);
+    rake2X = winMouseX - (cos(angle1) * segLength);
+    rake2Y = winMouseY - (sin(angle1) * segLength);
 
     segment(rake2X, rake2Y, angle1, img_rake2)
   }
@@ -307,8 +308,14 @@ function reset() {
 }
 
 function windowResized() {
-    removeElements();
-    setup();
+  removeElements();
+  resizeCanvas(windowWidth, windowHeight);
+  bLayer.width = windowWidth;
+  bLayer.height = windowHeight;
+  pLayer.width = windowWidth;
+  pLayer.height = windowHeight;
+  findLongEdge();
+  writeTextUI();
     //image(img_background, 0, 0, width, height);
 
 
